632bc07e11ba6f7a943c58dc9c4611c766e00680
Ronika Bhatti <ronika.bhatti@kellogg.northwestern.edu>
2023-05-18 01:15:37 +0000

    notes1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,25 @@ class SessionsController < ApplicationController
   def create
     # authenticate the user
     # 1. try to find the user by their unique identifier
+    @user = User.find_by({"email"=> params["email"]})
+
     # 2. if the user exists -> check if they know their password
+    if @user != nil
+
     # 3. if they know their password -> login is successful
+      if BCrypt::Password.new(@user["password"]) == params["password"] #@user["password"]
+        flash["notice"] = "Welcome."
+        redirect_to "/companies"
+      else 
+        flash["notice"] = "Nope."
+        redirect_to "/sessions/new"
+      end
     # 4. if the email does not exist or they do not know their password -> login fails
-    flash["notice"] = "Nope."
-    redirect_to "/sessions/new"
+    else 
+      flash["notice"] = "Nope."
+      redirect_to "/sessions/new"
   end
+end
 
   def destroy
     # logout the user
--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -4,5 +4,14 @@
 
 # TODO:
 # 1. encrypt a secret and puts encrypted string
+secret = "tacos"
+encrypted_secret = BCrypt::Password.create(secret)
+puts encrypted_secret
+
 # 2. prepare encrypted string for testing
+
+user_entered_secret = "tacos" #"puppies"
+result = BCrypt::Password.new(encrypted_secret) == user_entered_secret
+
 # 3. test secret against prepared encrypted string
+puts result
